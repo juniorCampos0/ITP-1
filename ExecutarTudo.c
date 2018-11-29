c55f6f73d06ee83fbaff179266213f306af89de4
Francisco <campos.junior.ifrn@gmail.com>
2018-11-28 22:33:31 -0300

    Versao final.

--- ExecutarTudo.c
+++
@@ -1,6 +0,0 @@
-# include <stdlib.h>
-
-int main (){
-	system ("gcc Interface.c Headers/CriaTabela.c Headers/CriaLinha.c Headers/ListaDados.c Headers/ProcuraString.c Headers/ApagaValor.c Headers/ApagaTabela.c Headers/CriaColuna.c -o ex -g -w");
-	return 0;
-}
